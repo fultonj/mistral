fd0ba9ecac5fbdd0cb9e805d2949ff213cdae3eb
John Fulton <fulton@redhat.com>
2017-02-02 16:40:15 +0000

    update ansible-test todo three tasks based on three ansible modules and updated the shell script that runs it to get the result of each task

--- ansible/ansible-test.sh
+++ ansible/ansible-test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 WORKFLOW='ansible-test'
+date > /tmp/file.txt
 
 source ~/stackrc
 EXISTS=$(mistral workflow-list | grep $WORKFLOW | wc -l)
@@ -12,4 +13,9 @@ fi
 mistral execution-create $WORKFLOW
 UUID=$(mistral execution-list | grep $WORKFLOW | awk {'print $2'} | tail -1)
 mistral execution-get $UUID
-mistral execution-get-output $UUID
+echo "Getting output for the following tasks in workflow $WORKFLOW"
+mistral task-list $UUID
+for TASK_ID in $(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$'); do
+    mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+done
+# mistral execution-get-output $UUID
