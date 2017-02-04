97c6abedaa9cdad172c10cf07e351a2aa09dd114
John Fulton <fulton@redhat.com>
2017-02-04 19:19:44 +0000

    rename ceph-ansible to mistral-ceph-ansible and import mistral-ceph-ansible.{sh,yaml} to modify

---
+++ mistral-ceph-ansible/init.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+FORK=0
+HACK=0
+CLONE=1
+
+if [ $FORK -eq 1 ]; then
+    # (OLD) use fork given https://github.com/d0ugal/mistral-ansible-actions/pull/1
+    git clone https://github.com/fultonj/mistral-ansible-actions.git
+    sudo rm -Rf /usr/lib/python2.7/site-packages/mistral_ansible*
+    pushd mistral-ansible-actions
+    sudo python setup.py install
+    popd
+else
+    sudo yum install -y python-pip
+    sudo pip install mistral-ansible-actions;
+fi    
+sudo mistral-db-manage populate;
+sudo systemctl restart openstack-mistral*;
+mistral action-list | grep ansible
+
+echo "Try these:"
+echo "  mistral action-get ansible"
+echo "  mistral action-get ansible-playbook"
+
+if [ $HACK -eq 1 ]; then
+    # use this workaround for now
+    id mistral
+    if [ ! -d /home/mistral ]; then
+	sudo mkdir /home/mistral
+    fi
+    sudo chown mistral:mistral /home/mistral/
+    sudo cp -r ~/.ssh/ /home/mistral/
+    sudo chown -R mistral:mistral /home/mistral/.ssh/
+fi
+
+if [ $CLONE -eq 1 ]; then
+    git clone git@github.com:ceph/ceph-ansible.git
+fi
---
+++ mistral-ceph-ansible/mistral-ceph-ansible.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+WORKFLOW='mistral-ceph-ansible'
+date > /tmp/file.txt
+cp -f hello_playbook.yaml /tmp/
+
+source ~/stackrc
+EXISTS=$(mistral workflow-list | grep $WORKFLOW | wc -l)
+if [[ $EXISTS -gt 0 ]]; then
+   mistral workflow-update $WORKFLOW.yaml
+else
+   mistral workflow-create $WORKFLOW.yaml    
+fi
+mistral execution-create $WORKFLOW
+UUID=$(mistral execution-list | grep $WORKFLOW | awk {'print $2'} | tail -1)
+mistral execution-get $UUID
+echo "Getting output for the following tasks in workflow $WORKFLOW"
+mistral task-list $UUID
+for TASK_ID in $(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$'); do
+    mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+done
+# mistral execution-get-output $UUID
