13b66be2c82c3f17076d40d88c9a896d8a91ee0c
John Fulton <fulton@redhat.com>
2017-02-07 18:47:28 +0000

    update purge-ceph-mistral.sh for rename; get workflow name correct

--- mistral-ceph-ansible/purge-ceph-mistral.sh
+++ mistral-ceph-ansible/purge-ceph-mistral.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Filename:                mistral-ceph-ansible.sh
 # Description:             prep and run ceph-ansible
-# Time-stamp:              <2017-02-07 13:19:21 jfulton> 
+# Time-stamp:              <2017-02-07 13:47:13 jfulton> 
 # -------------------------------------------------------
 RUN=1
-WORKFLOW='mistral-ceph-ansible-purge'
+WORKFLOW='purge-ceph-ansible'
 # -------------------------------------------------------
 if [ ! -f /tmp/ceph-ansible/purge-cluster.yml ]; then
   # workaround since this playbook expects files in parent directory
