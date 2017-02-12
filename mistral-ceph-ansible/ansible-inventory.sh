879958c108cb024037d42b2e247b64c280b9d94f
John Fulton <fulton@redhat.com>
2017-02-12 01:21:20 +0000

    update ansible-inventory.sh
    If there are no mons/osds, assume they are compute/control and add alias entry for them

--- mistral-ceph-ansible/ansible-inventory.sh
+++ mistral-ceph-ansible/ansible-inventory.sh
@@ -2,7 +2,7 @@
 # Filename:                ansible-inventory.sh
 # Description:             Builds an Ansible Inventory
 # Supported Langauge(s):   GNU Bash 4.2.x
-# Time-stamp:              <2017-02-04 12:45:55 jfulton> 
+# Time-stamp:              <2017-02-11 20:18:42 jfulton> 
 # -------------------------------------------------------
 echo "(re)building ansbile inventory"
 source ~/stackrc
@@ -24,6 +24,24 @@ for type in ${TYPES[@]}; do
     done
     echo "" >> /tmp/inventory
 done
+
+HACK=1
+# if there are no mons/osds, assume they are compute/control and add alias
+if [ $HACK -eq 1 ]; then
+    declare -A MAP=( [osds]=computes [mons]=controls )
+    for K in "${!MAP[@]}"; do
+	match=$(fgrep ${MAP[$K]} /tmp/inventory -A 1 | grep 192 | wc -l)
+	if [ $match -gt 0 ]; then
+	    line=$(grep ${MAP[$K]} /tmp/inventory -A 1 | tail -1)
+	    if [[ -n $line ]] ; then
+		sed -i "/$K/d" /tmp/inventory
+		echo [$K] >> /tmp/inventory
+		echo $line >> /tmp/inventory
+		echo "" >> /tmp/inventory
+	    fi
+	fi
+    done
+fi
 
 sudo mv /tmp/inventory /etc/ansible/hosts
 
